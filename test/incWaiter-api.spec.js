5b2457b0a946000df14fd892a2712e3bebb3a9f7
Bruno Morel <bruno.yvan.morel@gmail.om>
2013-07-19 18:02:15 +0200

    Modified Storage Persistent MySQL tests and added muAPI library

---
+++ test/incWaiter-api.spec.js
@@ -0,0 +1,38 @@
+/**
+ * Date: 2013-07-14
+ * User: bruno
+ *
+ * the muJasmine BDD test for Incredible Waiter API
+ *
+ */
+
+describe( "Incredible Waiter", function()
+{
+	var incWaiter = mu.require( '../../src/incWaiter.js' );
+
+	it( "mu is defined and working", function()
+	{
+		expect( mu ).toBeDefined( );
+		expect( mu.require ).toBeDefined( );
+	});
+
+	it( "the schema is loaded and works", function()
+	{
+		expect( Dish ).toBeDefined( );
+		expect( Ingredient ).toBeDefined( );
+	});
+
+	it( "the schema create the right type of objects (we just test Dish and Ingredient", function()
+	{
+		var newDish = new Dish( { 'name' : "testName", 'longdescription' : "testLongDescription" } );
+		var newIngredient = new Ingredient( );
+
+		expect( newDish.containBerry ).toBeDefined( );
+		expect( newDish.containNut ).toBeDefined( );
+		expect( newDish.name ).toEqual( "testName" );
+
+		newDish.relInstance( );
+		newIngredient.relInstance( );
+	});
+
+});
